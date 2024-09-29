5cb2a18ba4eeeeac500fcdb2812f909fdbb4a86d
Caleb Bae <baecal000@gmail.com>
2024-09-29 18:29:33 -0500

    Create dashboard

--- app/users/[id]/page.tsx
+++ app/users/[id]/page.tsx
@@ -1,24 +1,28 @@
-import UserForm from "@/components/UserForm";
-import prisma from "@/prisma/db";
 import React from "react";
+import prisma from "@/prisma/db";
+import UserForm from "@/components/UserForm";
+import { getServerSession } from "next-auth";
+import options from "../../api/auth/[...nextauth]/options";
 
 interface Props {
   params: { id: string };
 }
 
 const EditUser = async ({ params }: Props) => {
+  const session = await getServerSession(options);
+
+  if (session?.user.role !== "ADMIN") {
+    return <p className="text-destructive">Admin access required.</p>;
+  }
+
   const user = await prisma?.user.findUnique({
     where: { id: parseInt(params.id) },
-    select: {
-      id: true,
-      name: true,
-      username: true,
-      role: true,
-    },
   });
+
   if (!user) {
-    return <p className="text-destructive">User Not Found.</p>;
+    return <p className=" text-destructive">User Not Found.</p>;
   }
+
   user.password = "";
   return <UserForm user={user} />;
 };
--- components/MainNavLinks.tsx
+++ components/MainNavLinks.tsx
@@ -4,28 +4,30 @@ import Link from "next/link";
 import { usePathname } from "next/navigation";
 import React from "react";
 
-const MainNavLinks = () => {
+const MainNavLinks = ({ role }: { role?: string }) => {
   const links = [
-    { label: "Dashboard", href: "/" },
-    { label: "Tickets", href: "/tickets" },
-    { label: "Users", href: "/users" },
+    { label: "Dashboard", href: "/", adminOnly: false },
+    { label: "Tickets", href: "/tickets", adminOnly: false },
+    { label: "Users", href: "/users", adminOnly: true },
   ];
 
   const currentPath = usePathname();
   // console.log(currentPath);
   return (
     <div className="flex items-center gap-5">
-      {links.map((link) => (
-        <Link
-          key={link.label}
-          href={link.href}
-          className={`navbar-link ${
-            currentPath == link.href && " text-primary hover:text-primary/80"
-          }`}
-        >
-          {link.label}
-        </Link>
-      ))}
+      {links
+        .filter((link) => !link.adminOnly || role === "ADMIN")
+        .map((link) => (
+          <Link
+            key={link.label}
+            href={link.href}
+            className={`navbar-link ${
+              currentPath == link.href && " text-primary hover:text-primary/80"
+            }`}
+          >
+            {link.label}
+          </Link>
+        ))}
     </div>
   );
 };
